ce432092fd0fc0ff3b712eb58cf1ae94735c102b
Daniel <fintinariu.daniel@gmail.com>
2022-02-28 16:26:11 +0100

    Removed some console log

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -29,7 +29,6 @@ class Signin extends React.Component {
       .then(response => response.json())
       .then(user => {
         if (user.id) {
-          console.log(user)
           this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
